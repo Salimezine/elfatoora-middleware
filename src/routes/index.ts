ba934fb1c6a4b2f4008ad4178a36abdd8eb5f664
Kharrat M. <kharrat.m@tekru.net>
2026-01-21 14:11:09 +0100

    Remove webhooks route from main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import { Router, type Router as ExpressRouter } from "express";
 import documentsRouter from "./document.routes.js";
-import { webhooksRouter } from "./webhooks.routes.js";
 
 export const router: ExpressRouter = Router();
 
 router.use("/documents", documentsRouter);
-router.use("/webhooks", webhooksRouter);
